d1d4cdd6063c2886351fa86e2f088aaa5815f9e0
DESKTOP-T0B6IDL\romar <romaris.caamano.christian@gmail.com>
2022-03-24 23:33:50 +0100

    Add new register page

--- app/src/main/java/com/app/carvault/RegisterActivity.kt
+++ app/src/main/java/com/app/carvault/RegisterActivity.kt
@@ -4,21 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.register_activity)
         // Find the login button & launch new activity
-        val login_button = findViewById<Button>(R.id.login_button)
-        login_button.setOnClickListener {
-            val intent = Intent(this, NavDrawer::class.java)
-            startActivity(intent)
-        }
         val register_button = findViewById<Button>(R.id.register)
         register_button.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
+            Toast.makeText(this.applicationContext, "Register!", Toast.LENGTH_SHORT).show()
         }
     }
 }
